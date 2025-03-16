0f4c8d74c74ddc2a2f7a29e7f8bdd3fbd9a58091
modcomlearning <modcomlearning@gmail.com>
2025-03-17 00:27:00 +0300

    Final

--- README.md
+++ README.md
@@ -1425,6 +1425,67 @@ In this step we will be getting all products posted on the API. The get products
 In your React App Open Getproducts.js
 <br>
 
+```jsx
+const Getproducts = () => {
+    return ( 
+        <div>
+            <h1>Welcome to Get Products Components</h1>
+        </div>
+     );
+}
+
+export default Getproducts;
+```
+
+Create a Card to demonstrate how a single product will display, Update the HTML code as follows<br>
+
+```jsx
+
+const Getproducts = () => {
+
+
+    return (
+        <div className="row">
+            <h3 className="mt-5">Available Products</h3>
+ 
+            <div className="col-md-3 justify-content-center mb-4">
+                {/* Card with equal size */}
+                <div className="card shadow card-margin">
+
+                        <img 
+                            className=" product_img mt-4"
+                            src="logo.png"
+                        />
+                    {/* Card Body with a dummy Product */}
+                    <div className="card-body">
+                        <h5 className="mt-2">Unga</h5>
+                        <p className="text-muted">This is Nice Soft Ugali</p>
+                        <b className="text-warning">200 KES</b>  <br />
+
+                    </div>
+                </div>
+            </div>
+    </div>
+    );
+
+}
+
+export default Getproducts;
+```
+
+Run your App  <br>
+Runs the app in the development mode.\
+Open [http://localhost:3000/](http://localhost:3000/) to view it in your browser.
+<br>
+
+Output<br>
+Below shows a Card View of how a Product will be displayed<br>
+This a Dummy/Example Product, Not the ones from the Database <br>
+
+![alt text](image-27.png)
+
+Next, we need to fetch/get all products from our getproducts Backend API and display in the Card we've just seen<br>
+
 
 Import the required modules 
 
@@ -1510,7 +1571,7 @@ Inside use Effect, we Call getproducts() function to fetch all products.
 <br>
 
 
-in JSX, do below code. <br>
+in JSX that we had done earlier, we loop/map through the products from the API and display them in the Card, Update the Card by Removing the Dummy/Example product and bind with the products from your database. Do below code. <br>
 
 ```jsx
 return (
--- src/components/Getproducts.js
+++ src/components/Getproducts.js
@@ -1,79 +1,28 @@
-import { useState, useEffect } from "react"; // for state management
-import axios from "axios"; //For API Access
-import { Link, useNavigate, useLocation} from "react-router-dom"; // For link to other component
 
 const Getproducts = () => {
-
-    // Initialize Hooks
-    const [products, setProducts] = useState([]);  // Default to empty array instead of a string
-    const [loading, setLoading] = useState(""); //For loading message
-    const [error, setError] = useState(""); //error message hook
-    const navigate = useNavigate();  //For navigation to another component
-    
-    //Specify image location URL
-    const img_url = "https://modcom2.pythonanywhere.com/static/images/"
-    
-    const getproducts = async()=>{
-        setLoading("Please wait, We are retrieving the products .."); // Set loading message when fetching starts
-        try {
-        const response = await axios.get("https://modcom2.pythonanywhere.com/api/get_product_details")
-        setProducts(response.data)
-        setLoading("")
-        }
-        catch(error){
-            setLoading("")
-            setError("There was an Error")    
-        }
-    }//end function
-
-    // Call getproducts on Use Effect
-    useEffect(() => {
-       getproducts()
-    }, []); // empty dependency array ensures this runs only once when the component mounts
 
 
     return (
         <div className="row">
-
-         <nav className="m-4">
-            <Link to='/addproduct' className="btn btn-dark mx-2">UPLOAD PRODUCTS</Link>
-            <Link to='/signin' className="btn btn-dark mx-2">Sign In</Link>
-            <Link to='/signup' className="btn btn-dark mx-2">Sign Up</Link>
-        </nav>
-
-         <h3 className="mt-5">Available Products</h3>
-
-        {/* Bind Error Messages */}
-          {loading}
-          {error}
-
-        {/* Map over products and display them */}
-
-        {products.map((product) => (
+            <h3 className="mt-5">Available Products</h3>
+ 
             <div className="col-md-3 justify-content-center mb-4">
                 {/* Card with equal size */}
                 <div className="card shadow card-margin">
+
                         <img 
                             className=" product_img mt-4"
-                            src={img_url + product.product_photo} 
+                            src="logo.png"
                         />
-                  
+                    {/* Card Body with a dummy Product */}
                     <div className="card-body">
-                        <h5 className="mt-2">{product.product_name}</h5>
-                        <p className="text-muted">{product.product_description}</p>
-                        <b className="text-warning">{product.product_cost} KES</b>  <br />
+                        <h5 className="mt-2">Unga</h5>
+                        <p className="text-muted">This is Nice Soft Ugali</p>
+                        <b className="text-warning">200 KES</b>  <br />
 
-                        {/* We parse our Product to makepayment Component */}
-                        <button 
-                                className="btn btn-dark mt-2 w-100"
-                                onClick={() => navigate('/makepayment', { state: { product } })} // Directly pass product via state
-                            >
-                                Purchase Now
-                       </button>
                     </div>
                 </div>
             </div>
-        ))}
     </div>
     );
 
